861275e5e9206777e83b76869dc3f2c5799deee3
Yatarox <Faboto@yahoo.com>
2024-01-31 12:12:47 +0100

    recommencer

--- hello.py
+++ hello.py
@@ -1,4 +1,19 @@
-nombre = int(input("donner un nombre "))
-print(nombre)
-for i in range (11):
-    print(nombre*i)
+
+recommencer = False
+
+while not recommencer:
+    nombre = int(input("Donne un nombre : "))
+    print(nombre)
+
+    for i in range(11):
+        print(nombre * i)
+
+    choix = input("Veux-tu recommencer? (Oui/Non) ").lower()
+
+    if choix == "oui":
+        recommencer = False
+    elif choix == "non":
+        recommencer = True
+        print("C'est fini")
+    else:
+        print("Choix non valide. On recommence.")
